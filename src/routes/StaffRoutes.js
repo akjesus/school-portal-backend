4c28ff9d0c0261db6e68553c22b3898bdb11673e
Otong Akan <cutytongy@gmail.com>
2026-05-13 14:57:36 +0100

    added pm2

--- src/routes/StaffRoutes.js
+++ src/routes/StaffRoutes.js
@@ -1,10 +1,10 @@
 //geberate the staff routes for the school portal backend
 const router = require("express").Router();
 const staffController = require("../controllers/StaffController");
+const verifyToken = require("../controllers/AuthController")
 
 router.post("/", staffController.createStaff);
 router.get("/", staffController.getAllStaff);
-// router.get("/:id", staffController.getStaffById);
 
 
 module.exports = router;
